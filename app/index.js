d8c9f3bf11741a417bef891067b51f6cccd404f9
Aditi Sulke <aditi@Aditis-MacBook-Air.local>
2026-04-09 03:29:02 +0530

    initial clean project (app + terraform)

---
+++ app/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({
+    timestamp: new Date(),
+    ip: req.ip
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
